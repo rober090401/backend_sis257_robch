3e175eef910ef5fc9e70d1feeb86de9a1edf2d26
Mabel <colquefigueroamaribeljovita@gmail.com>
2025-11-13 19:30:13 -0400

    correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,6 +23,7 @@ import { AuthModule } from './auth/auth.module';
       entities: [__dirname + '*/**/entities/*.{ts|js}'],
       synchronize: true,
       autoLoadEntities: true,
+      ssl: { rejectUnauthorized: false },
     }),
     ArtistasModule,
     GenerosModule,
